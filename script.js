f172a0b0daa3a55219c9bc084f7e26aa051f815c
Rahini <rahinikalra6@gmail.com>
2026-02-17 11:04:17 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// repl---- read execute print loop 
+// console.log(a);
+// var a=12;
+// console.log(a)
+// datatypes = primitive , reference 
+// primitive 
+// numbers 
+// symbol
+// string 
+// big int 
+// boolean 
+// null
+// undefined 
+// big int
+//reference
+// arrays
+// objects 
+// functions
+// there are three ways to define variable and identifiers
+// let a = 123;
+// console.log(typeof a);
+// let b =123456789123456789;
+// console.log(b);
+// let c= BigInt(123456789123456789);
+// console.log(c);
+// let a ="     rahini     ";
+// console.log(a);
+// console.log(a[6]);
+// console.log(a.slice(0,8));
+// console.log(a.trim());
+// console.log(a.trimStart());
+// console.log(a.trimEnd());
+// // multi methods chaining((
+// console.log(a.trim().toUpperCase());
+// let a =false;
+// console.log(typeof a);
+// let b = {Name : "rahini"};
+// console.log(typeof b["name1"]);
+// let b = null;
+// console.log(typeof b);
+// let a = Symbol("rahini");
+// let b = Symbol("rahini");
+// console.log(a==b);
+// console.log(a);
