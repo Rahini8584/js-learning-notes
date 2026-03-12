2d72e23edd478e6a2976b7954776f778c80cadbf
Rahini <rahinikalra6@gmail.com>
2026-03-12 10:06:47 +0530

    shallow copy and deep copy

--- script6.js
+++ script6.js
@@ -4,7 +4,47 @@
 // document.body.innerHTML="<h1>RAHINI</h1>";
 
 
-var a = [ "apple" ,"mango", "vm", ["potato","tomato"]];
-var copyofa = [...a];
-a[3].pop();
-console.log(copyofa);
+// var a = [ "apple" ,"mango", "vm", ["potato","tomato"]];
+// var copyofa = [...a];
+// a[3].pop();
+// console.log(copyofa);
+
+// const myobj ={
+//     username :"Rahini",
+//     hobby : {
+//         hobby1 : "sleeping",
+//     },
+// };
+
+// const copy ={...myobj};
+// myobj.username  ="Rahini kalra";
+// myobj.hobby.hobby1="eating";
+// console.log(copy);
+
+
+// const myobj ={
+//     username :"Rahini",
+//     hobby : {
+//         hobby1 : "sleeping",
+//     },
+// };
+
+// const copy =JSON.parse(JSON.stringify(myobj));
+// myobj.username  ="Rahini kalra";
+// myobj.hobby.hobby1="eating";
+// console.log(copy);
+
+// const myobj ={
+//     username :"Rahini",
+//     hobby : {
+//         hobby1 : "sleeping",
+//     },
+//     greet : function(){
+//         console.log("hello");
+//     },
+// };
+
+// const copy =structuredClone(myobj);
+// myobj.username  ="Rahini kalra";
+// myobj.hobby.hobby1="eating";
+// console.log(copy);
