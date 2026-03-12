ddd5218dd8211b3958d31dc2ce4ddb030b16cd29
Rahini <rahinikalra6@gmail.com>
2026-03-12 10:45:14 +0530

    video access

--- script6.js
+++ script6.js
@@ -48,3 +48,19 @@
 // myobj.username  ="Rahini kalra";
 // myobj.hobby.hobby1="eating";
 // console.log(copy);
+  
+
+// const video = document.getElementById("rahini");
+// navigator.mediaDevices.getUserMedia({video:true}).then((stream)=>{video.srcObject=stream;})
+
+
+// const camera = document.getElementById('camera')
+// navigator.mediaDevices.getUserMedia({video: true}).then((stream)=>{
+//     camera.srcObject = stream
+// })
+
+const video = document.getElementById("nikhal");
+
+navigator.mediaDevices.getUserMedia({video:true}).then((stream)=>{
+    video.srcObject = stream;
+})
