fa5584db97454d9cf01cef89ee5b89e9b135825d
Rahini <rahinikalra6@gmail.com>
2026-03-31 10:06:04 +0530

    js

--- script7.js
+++ script7.js
@@ -1,1 +1,7 @@
-setTimeout
+const heading = document.getElementsByClassName("heading");
+for (var i = 0; i<=heading.length -1;i++) {
+    heading[i].innerHTML ='aditiya';
+    heading[i].style.width ='100px';
+    heading[i].style.color ='red';
+    heading[i].style.backgroundColor ='yellow';
+}
