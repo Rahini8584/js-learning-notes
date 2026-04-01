2146905cdace442df9a5aae118132b13cf69bca6
Rahini <rahinikalra6@gmail.com>
2026-04-01 12:04:12 +0530

    dom

---
+++ script8.js
@@ -0,0 +1,20 @@
+// const input = prompt("enter your age");
+// if(input >= 18){
+//     // document.body.innerHTML ="<h1 style='color:black' >you are eligible to drive </h1>";
+//     document.body.innerHTML ="<h1 id='rahini' >you are eligible to drive </h1>";
+//     document.getElementById("rahini").style.color="green";
+//     //document.body.innerText ="<h1>you are eligible to drive </h1>";
+//     //document.body.textContent ="<h1>you are eligible to drive </h1>";
+//     // document.body.style.color ="blue";
+// }
+// else{
+//     document.body.innerHTML ="<h1>you are NOT eligible to drive</h1>";
+//     document.body.style.color="red";
+// }
+// const a = document.querySelectorAll("h1");
+// console.log(a);
+// for (var i = 0; i < a.length; i++) {
+//     a[i].style.color = "red";
+//     a[i].style.backgroundColor = "black";
+//     a[i].style.textAlign = "center";
+// }
